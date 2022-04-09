f9c3d7806464f76b2bcb23c199fd14df19c64a64
Camilo Rodriguez <andrescamilocr482@gmail.com>
2022-04-09 12:54:23 -0500

    Database update

--- database/db.sql
+++ database/db.sql
@@ -17,16 +17,36 @@
 CREATE DATABASE IF NOT EXISTS `database` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `database`;
 
+-- Volcando estructura para tabla database.products
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name_product` varchar(150) NOT NULL,
+  `description` varchar(150) NOT NULL,
+  `amount` int(11) NOT NULL DEFAULT '0',
+  `price` int(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla database.products: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` (`id`, `name_product`, `description`, `amount`, `price`) VALUES
+	(1, 'Computador Acer', 'Portatil Acer 8GB RAM', 5, 1500000);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+
 -- Volcando estructura para tabla database.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `email` varchar(250) NOT NULL DEFAULT '',
   `password` varchar(250) NOT NULL DEFAULT '',
+  `status` varchar(50) NOT NULL,
+  `token` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla database.users: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
